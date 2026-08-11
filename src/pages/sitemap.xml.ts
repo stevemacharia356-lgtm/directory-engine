d930eebc53edbc66a11073965bd0c44c7226f41a
stevehungi <stevemacharia356@gmail.com>
2026-08-10 22:31:42 -0400

    Update sitemap with individual listing pages

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,16 +1,31 @@
 import type { APIRoute } from 'astro';
-import { getAllDirectorySlugs } from '../lib/firestore';
+import { getAllDirectorySlugs, getDirectoryData } from '../lib/firestore';
 
 export const GET: APIRoute = async () => {
   const slugs = await getAllDirectorySlugs();
   
-  const urls = slugs.map(slug => 
-    `  <url>
+  let urls = '';
+  
+  for (const slug of slugs) {
+    // Directory page
+    urls += `  <url>
     <loc>https://mymaindomain.co.ke/${slug}/</loc>
     <changefreq>daily</changefreq>
     <priority>0.8</priority>
-  </url>`
-  ).join('\n');
+  </url>\n`;
+    
+    // Individual listing pages
+    const data = await getDirectoryData(slug);
+    if (data && data.listings) {
+      for (const listing of data.listings) {
+        urls += `  <url>
+    <loc>https://mymaindomain.co.ke/${slug}/${listing.slug}/</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.7</priority>
+  </url>\n`;
+      }
+    }
+  }
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
